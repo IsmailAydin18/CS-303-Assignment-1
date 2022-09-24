b15e0b5dcd2eac8998a70c9b0903b5f983196f5f
IsmailAydin18 <77870165+IsmailAydin18@users.noreply.github.com>
2022-09-23 20:54:23 -0500

    Add files via upload

---
+++ cs 303 1/cs 303 1/Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+double newint(double num, double list[], int inarr);
+double check(double num, double list[], int inarr);
+double remove(double num, double list[],int inarr);
+double modify(double num, double num2, double list[], int inarr);
